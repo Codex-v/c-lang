21c5cef3fe9f85a7d9f631aae6e6579cdd29a854
ved <veds2103@gmail.com>
2023-04-10 00:55:41 +0530

    Added shell sort

---
+++ sorting/shellSort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void shellSort(int *arr,int n)
+{
+    int gap,i,j,key;
+
+    for(gap = n/2;gap > 0;gap /= 2)
+    {
+        for(i = gap;i<n;i++)
+        {
+            key = arr[i];
+            for( j = i;j>=gap && arr[j-gap]>key;j -= gap)
+            {
+                arr[j] = arr[j-gap];
+
+            }
+            arr[j] = key;
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {10, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("Original array: ");
+    printArray(arr, n);
+    
+    shellSort(arr, n);
+    
+    printf("Sorted array: ");
+    printArray(arr, n);
+    
+    return 0;
+}
