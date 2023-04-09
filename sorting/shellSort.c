02c29aadf369fcc92f4672fbe17e67368cfc1ab4
ved <veds2103@gmail.com>
2023-04-10 01:02:03 +0530

    ved's version of shell sort

--- sorting/shellSort.c
+++ sorting/shellSort.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
+void swap(int *a,int*b)
+{
+    *b = (*a+*b)-(*a=*b);
+}
+
 void shellSort(int *arr,int n)
 {
-    int gap,i,j,key;
+    int gap,i,j;
 
     for(gap = n/2;gap > 0;gap /= 2)
     {
         for(i = gap;i<n;i++)
         {
-            key = arr[i];
-            for( j = i;j>=gap && arr[j-gap]>key;j -= gap)
+            for(j = i-gap;j>=0;j-gap)
             {
-                arr[j] = arr[j-gap];
-
+                if(arr[j+gap]>arr[j])
+                    break;
+                else
+                {
+                    swap(&arr[j+gap],&arr[j]);
+                }
+                
             }
-            arr[j] = key;
         }
     }
 }
