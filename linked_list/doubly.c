958720510e1c63b490ce51c93549f30152b8634e
VEDS03 <veds2103@gmail.com>
2023-04-14 22:27:12 +0530

    updated the doubly linked list

--- linked_list/doubly.c
+++ linked_list/doubly.c
@@ -34,6 +34,30 @@ void insertAtFirst(int data)
     temp = newNode;
 }
 
+void insertAtmid(int data,int index)
+{   
+    temp = head;
+    Node * tonext = head->next;
+
+    for(int i = 1;i!=index-1;i++)
+    {
+        temp = temp->next;
+        tonext = tonext->next;
+
+
+    }
+
+    newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = temp->next;
+    temp->next = newNode;
+    newNode->prev = temp;
+    tonext->prev = newNode;
+    temp = newNode;
+
+
+}
+
 
 
 void NodeCreation(int data)
@@ -68,6 +92,7 @@ void main()
     NodeCreation(89);
     NodeCreation(90);
     NodeCreation(47);
+    insertAtmid(488,2);
     display();
 
 
